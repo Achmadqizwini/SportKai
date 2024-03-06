f824fb05a81802e774613e9f1a1f75f05089860c
Achmad Qizwini <akise.shun@gmail.com>
2024-03-06 21:20:24 +0700

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Achmadqizwini/SportKai/config"
+	"github.com/Achmadqizwini/SportKai/factory"
+	"github.com/Achmadqizwini/SportKai/middlewares"
+	"github.com/Achmadqizwini/SportKai/utils/database/mysql"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	db := mysql.InitDB(cfg)
+	// db := posgresql.InitDB(cfg)
+
+	e := echo.New()
+
+	factory.InitFactory(e, db)
+
+	// middleware
+	middlewares.LogMiddlewares(e)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.SERVER_PORT)))
+}
