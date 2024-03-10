409d7cfea828d8e2b4be8105b47f60ba09cd12da
Achmad Qizwini <akise.shun@gmail.com>
2024-03-10 22:40:14 +0700

    update public_id length

--- db/migrations/20240309033902_create.up.sql
+++ db/migrations/20240309033902_create.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user (
     id              INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    public_id       VARCHAR(20)  NOT NULL,
+    public_id       VARCHAR(40)  NOT NULL,
     fullname        VARCHAR(255) NOT NULL,
     email           VARCHAR(100) NOT NULL,
     password        VARCHAR(255) NOT NULL,
