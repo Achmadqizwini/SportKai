9892e46abe86e9a0e932453c6cfdd7e6a8b66dc8
Achmad Qizwini <akise.shun@gmail.com>
2024-03-22 10:43:28 +0700

    change model field type

--- features/clubMember/model/clubMember.go
+++ features/clubMember/model/clubMember.go
@@ -7,8 +7,8 @@ import (
 type ClubMember struct {
 	ID       uint
 	PublicId string
-	UserId   int    `json:"user_id" form:"user_id"`
-	ClubId   int    `json:"club_id" form:"club_id"`
+	UserId   uint   `json:"user_id" form:"user_id"`
+	ClubId   uint   `json:"club_id" form:"club_id"`
 	Status   string `json:"status" form:"status"`
 	JoinedAt time.Time
 	LeftAt   time.Time
