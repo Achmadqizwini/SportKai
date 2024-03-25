5383591cd05556c02f62cc32fade882db0b0b98b
Achmad Qizwini <akise.shun@gmail.com>
2024-03-25 22:24:21 +0700

    Update: modify query in create member

--- features/clubMember/repository/query.go
+++ features/clubMember/repository/query.go
@@ -3,15 +3,13 @@ package repository
 import (
 	"database/sql"
 	"errors"
-	"fmt"
-
 	"github.com/Achmadqizwini/SportKai/utils/logger"
 
 	model "github.com/Achmadqizwini/SportKai/features/clubMember/model"
 )
 
 type RepositoryInterface interface {
-	Create(input model.ClubMember) error
+	Create(input model.MemberPayload) error
 	Get() ([]model.ClubMember, error)
 	GetById(id string) (model.ClubMember, error)
 	Update(input model.ClubMember, id string) error
@@ -31,15 +29,27 @@ func New(db *sql.DB) RepositoryInterface {
 var logRepo = logger.NewLogger().Logger.With().Logger()
 
 // Create implements RepositoryInterface.
-func (u *memberRepository) Create(input model.ClubMember) error {
-	fmt.Println(input)
+func (u *memberRepository) Create(input model.MemberPayload) error {
+	type ID struct {
+		UserId uint
+		ClubId uint
+	}
+	var id ID
+	err := u.db.QueryRow(`
+		SELECT u.id AS user_id, c.id AS club_id 
+		FROM user u 
+		JOIN club c ON u.public_id = ? AND c.public_id = ?
+	`, input.UserId, input.ClubId).Scan(&id.UserId, &id.ClubId)
+	if err != nil {
+		return errors.New("error query statement")
+	}
 	stmt, errPrepare := u.db.Prepare("INSERT INTO club_member (public_id, user_id, club_id, status) VALUES (?, ?, ?, ?)")
 	if errPrepare != nil {
 		return errors.New("error prepare query statement")
 	}
 	defer stmt.Close()
 
-	_, errExec := stmt.Exec(input.PublicId, input.UserId, input.ClubId, input.Status)
+	_, errExec := stmt.Exec(input.PublicId, id.UserId, id.ClubId, input.Status)
 	if errExec != nil {
 		logRepo.Error().Err(errExec).Msg("error query execution")
 		return errors.New("error query execution")
@@ -84,7 +94,6 @@ func (u *memberRepository) GetById(id string) (model.ClubMember, error) {
 		"WHERE m.public_id=?", id)
 	err := row.Scan(&memberData.PublicId, &memberData.UserId, &memberData.ClubId, &memberData.Status)
 	if err != nil {
-		fmt.Println(err)
 		return model.ClubMember{}, errors.New("error parsing data to model")
 	}
 	return memberData, nil
