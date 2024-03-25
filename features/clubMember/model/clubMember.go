eb6d0129d4b728bb3cdbdd7ee320b1927534e9f8
Achmad Qizwini <akise.shun@gmail.com>
2024-03-25 22:23:45 +0700

    Add: input member payload when create a new member

--- features/clubMember/model/clubMember.go
+++ features/clubMember/model/clubMember.go
@@ -7,9 +7,16 @@ import (
 type ClubMember struct {
 	ID       uint
 	PublicId string
-	UserId   uint   `json:"user_id" form:"user_id"`
-	ClubId   uint   `json:"club_id" form:"club_id"`
-	Status   string `json:"status" form:"status"`
+	UserId   uint
+	ClubId   uint
+	Status   string
 	JoinedAt *time.Time
 	LeftAt   *time.Time
+}
+
+type MemberPayload struct {
+	PublicId string
+	UserId   string `json:"user_id" form:"user_id"`
+	ClubId   string `json:"club_id" form:"club_id"`
+	Status   string `json:"status" form:"status"`
 }
